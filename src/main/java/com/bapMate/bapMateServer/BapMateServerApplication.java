07bf8bdd511311d5db5768944f6f754888a72025
Hanbee Lee <bee1162@naver.com>
2023-10-31 19:51:23 +0900

    setting :초기 설정

---
+++ src/main/java/com/bapMate/bapMateServer/BapMateServerApplication.java
@@ -0,0 +1,13 @@
+package com.bapMate.bapMateServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapMateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapMateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bapMate/bapMateServer/BapMateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bapMate.bapMateServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapMateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
