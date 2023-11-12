2bdcb2436eb985a255d80feb9d1f91724733d6ae
‘yj-leez’ <yj_leez@naver.com>
2023-11-12 17:01:19 +0900

    feat: userkeyword entity, repository

---
+++ src/main/java/com/bapMate/bapMateServer/domain/keyword/entity/Eating.java
@@ -0,0 +1,27 @@
+package com.bapMate.bapMateServer.domain.keyword.entity;
+
+import com.bapMate.bapMateServer.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Eating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "eating_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Amount amount;
+    @Enumerated(EnumType.STRING)
+    private Pace pace;
+
+}
---
+++ src/main/java/com/bapMate/bapMateServer/domain/keyword/entity/Pace.java
@@ -0,0 +1,5 @@
+package com.bapMate.bapMateServer.domain.keyword.entity;
+
+public enum Pace {
+    SLOW, MODERATE, FAST;
+}
