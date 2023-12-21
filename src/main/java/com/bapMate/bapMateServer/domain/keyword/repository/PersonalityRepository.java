90f44eade4ba9e1c8f4cccf120c06e46c77ce95a
‘yj-leez’ <yj_leez@naver.com>
2023-12-21 17:32:22 +0900

    feat: chatgpt request 요청

--- src/main/java/com/bapMate/bapMateServer/domain/keyword/repository/PersonalityRepository.java
+++ src/main/java/com/bapMate/bapMateServer/domain/keyword/repository/PersonalityRepository.java
@@ -1,7 +1,14 @@
 package com.bapMate.bapMateServer.domain.keyword.repository;
 
+import com.bapMate.bapMateServer.domain.keyword.entity.Hobby;
 import com.bapMate.bapMateServer.domain.keyword.entity.Personality;
+import com.bapMate.bapMateServer.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PersonalityRepository extends JpaRepository<Personality, Long> {
+    Personality findByUser(User user);
 }
